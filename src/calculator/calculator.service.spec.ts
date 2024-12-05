8bcf68a3df901d02398ab150461b6db46c9352b7
Arun Singh <mrarunsingh8@gmail.com>
2024-12-05 23:10:01 +0530

    Add new cases.

--- src/calculator/calculator.service.spec.ts
+++ src/calculator/calculator.service.spec.ts
@@ -23,6 +23,7 @@ describe('CalculatorService', () => {
 
   it('should return the sum of two numbers', () => {
     expect(service.add('1,2')).toBe(3);
+    expect(service.add('1,5')).toBe(6);
     expect(service.add('10,20')).toBe(30);
   });
 
